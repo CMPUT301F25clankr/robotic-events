e4fc8ac6562668da324cc149e1f4c2597c593570
kmacquarrie <nozaki.kouta1@gmail.com>
2025-11-25 06:05:55 -0700

    QR Code Scan activity, dependencies (not tested with real camera)

--- app/src/main/java/com/example/robotic_events_test/QRScanRedirect.java
+++ app/src/main/java/com/example/robotic_events_test/QRScanRedirect.java
@@ -6,13 +6,8 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
